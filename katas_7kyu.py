bb7b7e2d2919760e6a8569ef52748256d55d67d0
Ivan <ivan.bondarenko.it@gmail.com>
2023-01-01 21:42:27 +0400

    01.01.2023

--- katas_7kyu.py
+++ katas_7kyu.py
@@ -23,3 +23,45 @@ def accum(s):
 # Write a function which converts the input string to uppercase.
 def make_upper_case(s):
     return s.upper()
+
+
+# The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+#
+# To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+#
+# Input
+# Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+#
+# Output
+# Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+#
+# Example
+# input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+# output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+def open_or_senior(data):
+    return [['Open', 'Senior'][i[0] >= 55 and i[1] > 7] for i in data]
+
+
+# Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+#
+# Note: a and b are not ordered!
+#
+# Examples (a, b) --> output (explanation)
+# (1, 0) --> 1 (1 + 0 = 1)
+# (1, 2) --> 3 (1 + 2 = 3)
+# (0, 1) --> 1 (0 + 1 = 1)
+# (1, 1) --> 1 (1 since both are same)
+# (-1, 0) --> -1 (-1 + 0 = -1)
+# (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+def get_sum(a, b):
+    return sum(range(min(a, b), max(a, b) + 1))
+
+# Rock Paper Scissors
+# Let's play! You have to return which player won! In case of a draw return Draw!.
+#
+# Examples(Input1, Input2 --> Output):
+#
+# "scissors", "paper" --> "Player 1 won!"
+# "scissors", "rock" --> "Player 2 won!"
+# "paper", "paper" --> "Draw!"
+
