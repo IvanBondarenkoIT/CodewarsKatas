a49b77a14527bade0f6338fe47a3b8432a0de482
Ivan <ivan.bondarenko.it@gmail.com>
2023-01-05 22:58:26 +0400

    05.01.2023

--- katas_6kyu.py
+++ katas_6kyu.py
@@ -25,3 +25,18 @@ def tower_builder(n_floors):
         lst.append(f"{' ' * (n_floors - 1 - i)}{'*' * i}*{'*' * i}{' ' * (n_floors - 1 - i)}")
     return lst
 
+
+def solution(s):
+    """
+    Complete the solution so that the function will break up camel casing, using a space between words.
+    Example
+    "camelCasing"  =>  "camel Casing"
+    "identifier"   =>  "identifier"
+    ""             =>  ""
+    """
+    res = ''
+    for i in s:
+        if i.isupper():
+            res += ' '
+        res += i
+    return res
